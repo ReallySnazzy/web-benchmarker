11d87adf3de7950746c0126d0b1c52b48a59c524
Jonathan Cooper <me+benchbot@jonathan-cooper.com>
2025-11-29 11:27:11 -0800

    Remove test 14 with coredump

--- tests/14/devenv.nix
+++
@@ -1,6 +0,0 @@
-{ pkgs, lib, config, inputs, ... }:
-
-{
-  packages = [ pkgs.ponyc pkgs.pony-corral ];
-  processes.webserver.exec = "cd web && corral fetch && corral run -- ponyc -Dopenssl_3.0.x && ./web";
-}
